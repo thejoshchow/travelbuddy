4bed805b7b8a1af8b1e3932f5b93cf6958bf65b6
Joshua Chow <thejoshchow@gmail.com>
2024-01-17 17:40:01 -0700

    adds migration to add users table

---
+++ api/migrations/001_create_users_table.py
@@ -0,0 +1,22 @@
+steps = [
+    [
+
+        """
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY NOT NULL,
+            username VARCHAR(20) UNIQUE NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            phone VARCHAR(10),
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            display_name VARCHAR(100) NOT NULL,
+            picture_url VARCHAR(200)
+        );
+        """,
+        """
+        DROP TABLE users;
+        """
+    
+    ]
+]
