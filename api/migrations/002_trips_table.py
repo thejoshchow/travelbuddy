878cec941a5a30d9df0de61c6eccb91361f5e715
Darnell <darnellcalahan15@gmail.com>
2024-01-18 15:04:56 -0800

    creates trips table migration

---
+++ api/migrations/002_trips_table.py
@@ -0,0 +1,22 @@
+
+steps = [
+    [
+
+        """
+        CREATE TABLE trips (
+            trip_id SERIAL PRIMARY KEY NOT NULL,
+            name VARCHAR(20) NOT NULL,
+            location VARCHAR(100) NOT NULL,
+            start_date DATE,
+            end_date DATE,
+            picture_url VARCHAR (200),
+            owner INT REFERENCES users(id) NOT NULL
+
+        );
+        """,
+        """
+        DROP TABLE trips;
+        """
+
+    ]
+]
