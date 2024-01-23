6741cd1a2c6274afc280497ff1dccc4002c0846b
Joshua Chow <thejoshchow@gmail.com>
2024-01-23 20:26:14 +0000

    adds api endpoint to update user info

--- api/queries/users.py
+++ api/queries/users.py
@@ -1,23 +1,60 @@
+from typing import Optional
 from pydantic import BaseModel
+from fastapi import HTTPException
+
+from queries.pool import pool
 
 
-class UserBase(BaseModel):
-    username: str
-    email: str
-    phone: str
+class UserIn(BaseModel):
+    phone: Optional[str] = None
     first_name: str
     last_name: str
     display_name: str
-    picture_url: str
+    picture_url: Optional[str] = None
 
 
-class UserIn(UserBase):
-    password: str
-
-
-class UserOut(UserBase):
+class UserOut(UserIn):
     user_id: int
 
 
 class UserRepo:
-    pass
+    def update(self, user_form: UserIn, user_id: int):
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    result = cur.execute(
+                        """
+                        UPDATE users
+                        SET phone = %s,
+                            first_name = %s,
+                            last_name = %s,
+                            display_name = %s,
+                            picture_url = %s
+                        WHERE user_id = %s
+                        RETURNING
+                            phone,
+                            first_name,
+                            last_name,
+                            display_name,
+                            picture_url;
+                        """,
+                        [
+                            user_form.phone,
+                            user_form.first_name,
+                            user_form.last_name,
+                            user_form.display_name,
+                            user_form.picture_url,
+                            user_id,
+                        ],
+                    )
+                    user = result.fetchone()
+                    return UserOut(
+                        phone=user[0],
+                        first_name=user[1],
+                        last_name=user[2],
+                        display_name=user[3],
+                        picture_url=user[4],
+                        user_id=user_id,
+                    )
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"{e}")
--- api/routers/users.py
+++ api/routers/users.py
@@ -1,4 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from queries.users import UserIn, UserRepo, UserOut
 
 router = APIRouter()
+
+
+@router.put("/api/user/{user_id}")
+def update_user(
+    user_form: UserIn, user_id: int, users: UserRepo = Depends()
+) -> UserOut:
+    return users.update(user_form, user_id)
