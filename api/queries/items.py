468b1feec2c3d926ac26460661c8e0487dcb30f6
Aaron D. <jaaronduke@gmail.com>
2024-01-24 00:35:33 +0000

    finished update item endpoint

--- api/queries/items.py
+++ api/queries/items.py
@@ -25,7 +25,61 @@ class ItemOut(ItemIn):
     item_id: int
 
 
+class ItemUpdate(BaseModel):
+    author: int
+    category_id: int = 5
+    name: str
+    description: Optional[str] = None
+    scheduled: bool = False
+    url: Optional[str] = None
+    picture_url: Optional[str] = None
+    cost: Optional[Decimal] = None
+    cost_per_person: Optional[bool] = None
+    notes: Optional[str] = None
+
+
 class ItemRepository:
+    def update(self, trip_id, item_id, item: ItemUpdate) -> ItemOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        UPDATE items
+                        SET author = %s,
+                            category_id = %s,
+                            name = %s,
+                            description = %s,
+                            scheduled = %s,
+                            url = %s,
+                            picture_url = %s,
+                            cost = %s,
+                            cost_per_person = %s,
+                            notes = %s
+                        WHERE trip_id = %s AND item_id = %s
+                        RETURNING item_id;
+                        """,
+                        [
+                            item.author,
+                            item.category_id,
+                            item.name,
+                            item.description,
+                            item.scheduled,
+                            item.url,
+                            item.picture_url,
+                            item.cost,
+                            item.cost_per_person,
+                            item.notes,
+                            trip_id,
+                            item_id,
+                        ],
+                    )
+                    id = result.fetchone()[0]
+                    updated_data = item.dict()
+                    return ItemOut(trip_id=trip_id, item_id=id, **updated_data)
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"{e}")
+
     def create(self, trip_id, item: ItemIn) -> ItemOut:
         try:
             # connect to db
--- api/routers/items.py
+++ api/routers/items.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from fastapi import APIRouter, Depends
 
-from queries.items import ItemIn, ItemOut, ItemRepository
+from queries.items import ItemIn, ItemOut, ItemRepository, ItemUpdate
 from queries.errors import Error
 
 
@@ -14,3 +14,13 @@ def create_item(
     trip_id, item: ItemIn, repo: ItemRepository = Depends(ItemRepository)
 ) -> Union[ItemOut, Error]:
     return repo.create(trip_id, item)
+
+
+@router.put("/api/trip/{trip_id}/{item_id}")
+def update_item(
+    item_id: int,
+    trip_id: int,
+    item: ItemUpdate,
+    repo: ItemRepository = Depends(ItemRepository),
+):
+    return repo.update(trip_id, item_id, item)
