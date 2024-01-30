ceef9a87b7333abbf663cef60506281ad3b53892
Joshua Chow <thejoshchow@gmail.com>
2024-01-29 20:20:09 -0700

    adds item form

---
+++ ghi/src/pages/items/ItemsForm.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { useAddItemMutation } from "../../features/items/itemsApi";
+
+const ItemsForm = () => {
+    const [addItem, {}] = useAddItemMutation()
+    const [formData, setFormData] = useState({
+        category: 5,
+        name: '',
+        description: '',
+        url: '',
+        cost: '',
+        costPerPerson: true,
+        notes: ''
+    })
+    const handleFormChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+
+        })
+    }
+
+    const handleSubmit = async (e) => { 
+        e.preventDefault()
+        const { fulfilled } = await addItem(formData).unwrap()
+        console.log(fulfilled)
+    }
+
+    return (
+        <div className='container'>
+            <form onSubmit={handleSubmit}>
+                <select className="form-select mb-3">
+                    <option value=''>Select category</option>
+                </select>
+                <div className="mb-3">
+                    <input value={formData.name} onChange={handleFormChange} type="text" className="form-control" name="name" id="item-name" placeholder="Item name" />
+                </div>
+                <div className="mb-3">
+                    <textarea value={formData.description} onChange={handleFormChange} type="text" className="form-control" rows="3" id="item-description" name="description" placeholder="Item description"/>
+                </div>
+                <div className="mb-3">
+                    <input value={formData.url} onChange={handleFormChange} type="text" className="form-control" id="item-url" name="url" placeholder="Item url"/>
+                </div>
+                <div className="mb-3">
+                    <input value={formData.cost} onChange={handleFormChange} type="text" className="form-control" name="cost" id="item-cost" placeholder="Item cost"/>
+                </div>
+                <div className="form-check mb-3">
+                    <input value={true} onChange={handleFormChange} type="radio" className="form-check-input" id="item-costPerPerson" name="costPerPerson"/>
+                    <label className="form-check-label" htmlFor="costPerPerson1" >
+                        Per Person
+                    </label>
+                </div>
+                <div className="form-check mb-3">
+                    <input value={false} onChange={handleFormChange} type="radio" className="form-check-input" id="item-costPerPerson" name="costPerPerson"/>
+                    <label className="form-check-label" htmlFor="costPerPerson2" >
+                        For Group
+                    </label>
+                </div>
+                <div className="mb-3">
+                    <textarea value={formData.notes} onChange={handleFormChange} type="text" className="form-control" rows="3" id="item-notes" name="notes" placeholder="Notes"/>
+                </div>
+                <button className="btn btn-primary">Add Item</button>
+            </form>
+        </div>
+    )
+}
+
+export default ItemsForm;
