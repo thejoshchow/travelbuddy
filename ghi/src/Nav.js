8fe75f8aafb7d10e0a3186c58fcc0e823789804a
Meghan Hyde <meghanahyde@gmail.com>
2024-02-01 23:55:52 +0000

    Worked on a basic stylized navbar - still have elements later to implement

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -6,31 +6,36 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
       <div className="container-fluid">
-        <div className="nav-links">
-        <NavLink className="navbar-link" to="">Home</NavLink>
-        <NavLink className="navbar-link" to="signup">Signup!</NavLink>
+        <a class="logo" href="/">
+          <img src="logo-transparent-png.png" alt="" width="60" height="48"/>
+        </a>
+
+        <div className="nav-links ml-auto">
+          <NavLink className="navbar-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="navbar-link" to="signup">
+            Signup
+          </NavLink>
+          <NavLink className="navbar-link" to="token">
+            Login
+          </NavLink>
+          <NavLink className="navbar-link" to="login">
+            Login2
+          </NavLink>
         </div>
-        <button className="navbar-toggler" 
-                type="button" 
-                data-bs-toggle="collapse" 
-                data-bs-target="#navbarSupportedContent" 
-                aria-controls="navbarSupportedContent" 
-                aria-expanded="false" 
-                aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           </ul>
         </div>
       </div>
-     
     </nav>
-        
+
   );
 }
 
 export default Nav;
 
 
-          
+
+
