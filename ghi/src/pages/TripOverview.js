6f6c88c71441e929d200308651751c2d027992bc
Aaron D. <jaaronduke@gmail.com>
2024-02-01 17:53:55 -0500

    finished basic css layout for trip overview,left in borders for later

--- ghi/src/pages/TripOverview.js
+++ ghi/src/pages/TripOverview.js
@@ -3,6 +3,14 @@ import { useParams } from "react-router-dom"
 import { useGetOneTripQuery } from "../services/tripApi"
 import { useGetCategoriesQuery} from "../services/categoryApi"
 import { useGetBuddyQuery } from "../services/buddiesApi"
+import {Container, 
+        Row, 
+        Col, 
+        Card, 
+        Form, 
+        Button, } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css'
 
 const TripOverview = () => {
     const { trip_id } = useParams();
@@ -14,39 +22,49 @@ const TripOverview = () => {
     }
     
     return (
-        <>
-            <h1>Trip Overview</h1>
-            <div className="container d-flex wrap">
-                <div>
-                    <h5>Trip details</h5>
+    <>
+        
+        <div className="trip-overview"> <h1>Trip Overview</h1> </div>
+        <Container className="details">
+          
+            <Card style={{ width: '18rem' }} className="card-container-trip-details">
+                <Card.Body className="trip-detail-body">
+                    <Card.Title className="card-title-trip-details">Trip details</Card.Title>
                     <ul>
                         <li>{trip.name}</li> 
                         <li>{trip.location}</li>
                         <li>{trip.start_date}</li>
                         <li>{trip.end_date}</li>
                         <li>{trip.picture_url}</li> 
-                        
-
                     </ul>
-                </div>
-                {categories.map((category) =>(
-                    <div key={category.category_id}>
-                        {category.category_name}
-                    </div>
-                    
-                ))
-                }       
-                <div>
-                    <h5>Who's going</h5>
+                </Card.Body>
+                </Card>
+            <Card style={{ width: '18rem' }} className="card-container-buddies">
+                <Card.Body className="buddy-body">
+                    <Card.Title>Who's going</Card.Title>
                     {buddies.buddies.map((buddy) => (
-                        <div key={buddy.user_id}>
+                        <p key={buddy.user_id}>
                             {buddy.display_name}
-                        </div>
+                        </p>
                     ))}
-                </div>
-            </div>
-        </>
+                </Card.Body>
+                </Card>
+            </Container>
+              <Container className="categories">
+            {categories.map((category) =>(
+                <Card style={{ width: '18rem' }} className="card-container-categories" key={category.category_id}>
+                    <Card.Body className="category-body">
+                     <div className ="category-items"> {category.category_name} </div>  
+                    </Card.Body>
+                </Card>
+            ))}
+        </Container>
+    </>
     )
 }
+       
+        
+      
+
 
 export default TripOverview
