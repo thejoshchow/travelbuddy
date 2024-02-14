c9284c6eb2db640728ef5f26f25fc10a9340c985
Darnell <darnellcalahan15@gmail.com>
2024-02-14 12:16:52 -0800

    who's going auto rerender

--- ghi/src/services/buddiesApi.js
+++ ghi/src/services/buddiesApi.js
@@ -4,12 +4,14 @@ import { baseQuery } from "./baseQuery";
 export const buddiesApi = createApi({
     reducerPath: 'buddiesApi',
     baseQuery: baseQuery,
+    tagTypes: ["buddy"],
     endpoints: (builder) => ({
         getBuddy: builder.query({
             query: (trip_id) => ({
                 url: `/api/trip/${trip_id}/buddy`,
                 method: 'GET',
             }),
+            providesTags:["buddy"],
         }),
 
         addBuddy: builder.mutation({
@@ -17,7 +19,8 @@ export const buddiesApi = createApi({
                 url: `/api/trip/${info.trip_id}/buddy`,
                 method: 'POST',
                 body: info.form,
-            })
+            }),
+            invalidatesTags: ["buddy"],
         }),
         isBuddy: builder.query({
             query: (trip_id) => ({
