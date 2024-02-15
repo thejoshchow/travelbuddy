98fb3037b3713f4c01f24990d49dc93a0dee3762
Joshua Chow <thejoshchow@gmail.com>
2024-02-14 19:17:45 -0700

    removes migration file. updates item table name column to meet max 20 char

--- api/migrations/005_items_table.py
+++ api/migrations/005_items_table.py
@@ -6,7 +6,7 @@ steps = [
             trip_id INT REFERENCES trips ON DELETE CASCADE NOT NULL,
             author INT REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
             category_id INT DEFAULT 5 REFERENCES item_categories ON DELETE SET DEFAULT,
-            name VARCHAR(100) NOT NULL,
+            name VARCHAR(20) NOT NULL,
             description TEXT,
             scheduled BOOLEAN,
             url VARCHAR(200),
